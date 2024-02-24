15265178b899e267ef4facbcd4f92dce7369c151
Leonardo Covarrubias Lemus <leonardolemus216@gmail.com>
2024-02-23 23:05:23 -0600

    Commit Final

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -6,7 +6,7 @@ import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 
 class RegisterScreen extends StatefulWidget {
-  const RegisterScreen({super.key});
+  const RegisterScreen({Key? key}) : super(key: key);
 
   @override
   State<RegisterScreen> createState() => _RegisterScreenState();
@@ -15,8 +15,28 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   final formKey = GlobalKey<FormState>();
   File? _image;
+  final _contrasenaController = TextEditingController();
+  final _contrasenaFocusNode = FocusNode();
+  bool _showPasswordRequirements = false;
 
-  Future getImage(ImageSource source) async {
+  @override
+  void initState() {
+    super.initState();
+    _contrasenaFocusNode.addListener(() {
+      setState(() {
+        _showPasswordRequirements = _contrasenaFocusNode.hasFocus;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _contrasenaController.dispose();
+    _contrasenaFocusNode.dispose();
+    super.dispose();
+  }
+
+  Future<void> _getImage(ImageSource source) async {
     try {
       final image = await ImagePicker().pickImage(source: source);
       if (image == null) return;
@@ -36,91 +56,74 @@ class _RegisterScreenState extends State<RegisterScreen> {
     final txtNombre = TextFormField(
       decoration: InputDecoration(
         labelText: "Nombre",
-        labelStyle: const TextStyle(color: Colors.black),
-        errorStyle: const TextStyle(color: Colors.black),
+        labelStyle: TextStyle(color: Colors.black),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(5.0),
         ),
       ),
       validator: (value) {
-        if (value != null && value.isEmpty) {
+        if (value == null || value.isEmpty) {
           return 'Ingresa un nombre';
-        } else {
-          return null;
         }
+        return null;
       },
     );
 
     final txtCorreo = TextFormField(
       decoration: InputDecoration(
         labelText: "Correo Electronico",
-        labelStyle: const TextStyle(color: Colors.black),
-        errorStyle: const TextStyle(color: Colors.black),
+        labelStyle: TextStyle(color: Colors.black),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(5.0),
         ),
       ),
       validator: (value) {
-        if (value != null && !EmailValidator.validate(value)) {
-          return 'Ingresa un correo valido';
-        } else {
-          return null;
+        if (value == null || !EmailValidator.validate(value)) {
+          return 'Ingresa un correo válido';
         }
+        return null;
       },
     );
 
     final txtContrasena = TextFormField(
+      controller: _contrasenaController,
       obscureText: true,
+      focusNode: _contrasenaFocusNode,
       decoration: InputDecoration(
         labelText: "Contraseña",
-        labelStyle: const TextStyle(color: Colors.black),
-        errorStyle: const TextStyle(color: Colors.black),
+        labelStyle: TextStyle(color: Colors.black),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(5.0),
         ),
       ),
       validator: (value) {
-        if (value != null && value.isEmpty) {
+        if (value == null || value.isEmpty) {
           return 'Ingresa una contraseña';
-        } else {
-          return null;
+        } else if (value.length < 8) {
+          return 'La contraseña debe tener al menos 8 caracteres';
+        } else if (!RegExp(
+                r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}|<>?]).+$')
+            .hasMatch(value)) {
+          return 'La contraseña debe incluir letras y números, combinar mayúsculas y minúsculas, y contener caracteres especiales';
+        } else if (value.contains(' ')) {
+          return 'La contraseña no puede contener espacios en blanco';
         }
+        return null;
       },
     );
 
     final spaceHorizontal = SizedBox(
       height: 15,
     );
-    final spaceGiant = SizedBox(
-      height: 60,
-    );
-
-    final btnRegistro = ElevatedButton(
-      onPressed: () async {
-        if (formKey.currentState!.validate()) {}
-      },
-      style: ElevatedButton.styleFrom(
-        elevation: 10.0,
-        textStyle:
-            const TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 20),
-        fixedSize: const Size(800, 60),
-        backgroundColor:
-            const Color.fromARGB(255, 255, 255, 255), // Cambia el color aquí
-      ),
-      child: const Text('Registrarse'),
-    );
 
     final btnImagen = ElevatedButton(
       onPressed: () {
-        getImage(ImageSource.gallery);
+        _getImage(ImageSource.gallery);
       },
-      style: ElevatedButton.styleFrom(fixedSize: const Size(400, 50)),
       child: Row(
         children: [
           Icon(Icons.image_outlined),
-          SizedBox(
-            width: 20,
-          ),
+          SizedBox(width: 20),
           Text('Elige una foto de la galería')
         ],
       ),
@@ -128,68 +131,71 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
     final btnCamara = ElevatedButton(
       onPressed: () {
-        getImage(ImageSource.camera);
+        _getImage(ImageSource.camera);
       },
-      style: ElevatedButton.styleFrom(fixedSize: const Size(400, 50)),
       child: Row(
         children: [
           Icon(Icons.camera),
-          SizedBox(
-            width: 20,
-          ),
+          SizedBox(width: 20),
           Text('Realiza una foto con la camara')
         ],
       ),
     );
 
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        child: Padding(
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
           padding: const EdgeInsets.all(8.0),
           child: Form(
             key: formKey,
-            child: Stack(
-              alignment: Alignment.topCenter,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Column(
-                  // mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    spaceHorizontal,
-                    spaceHorizontal,
-                    spaceHorizontal,
-                    ClipOval(
-                      child: _image != null
-                          ? Image.file(
-                              _image!,
-                              width: 150,
-                              height: 150,
-                              fit: BoxFit.cover,
-                            )
-                          : Icon(
-                              Icons.person,
-                              size: 150,
-                            ),
-                    ),
-                    spaceHorizontal,
-                    spaceHorizontal,
-                    btnImagen,
-                    spaceHorizontal,
-                    btnCamara,
-                    spaceHorizontal,
-                    txtNombre,
-                    spaceHorizontal,
-                    txtCorreo,
-                    spaceHorizontal,
-                    txtContrasena,
-                    spaceHorizontal,
-                    spaceHorizontal,
-                    spaceHorizontal,
-                    btnRegistro,
-                    spaceGiant
-                  ],
+                spaceHorizontal,
+                spaceHorizontal,
+                ClipOval(
+                  child: _image != null
+                      ? Image.file(
+                          _image!,
+                          width: 150,
+                          height: 150,
+                          fit: BoxFit.cover,
+                        )
+                      : Icon(
+                          Icons.person,
+                          size: 150,
+                        ),
                 ),
+                spaceHorizontal,
+                btnImagen,
+                spaceHorizontal,
+                btnCamara,
+                spaceHorizontal,
+                txtNombre,
+                spaceHorizontal,
+                txtCorreo,
+                spaceHorizontal,
+                txtContrasena,
+                if (_showPasswordRequirements)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      ''' - Debe incluir letras y números.\n - Debe combinar letras mayúsculas y minúsculas.\n - La contraseña debe incluir caracteres especiales.\n - La longitud de la contraseña debe ser igual o mayor a 8 caracteres.\n - No debe tener espacios en blanco.''',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                spaceHorizontal,
+                ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      // Aquí va la lógica para el registro
+                    }
+                  },
+                  child: Text('Registrarse'),
+                ),
+                SizedBox(height: 100),
               ],
             ),
           ),
